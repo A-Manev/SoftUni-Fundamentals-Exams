36d139c6ee5d76f2f7062e35b0c3bccc43263a29
A-Manev <52919486+A-Manev@users.noreply.github.com>
2019-11-23 13:26:12 +0200

    Add files via upload

---
+++ 04. Final Exam - 14 April 2019 Group I/01. Arriving in Kathmandu/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _01._Arriving_in_Kathmandu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"^(?<peakName>[A-Za-z0-9\!\@\#\$\?]+)=(?<length>\d+)<<(?<code>.+)";
+
+            string input = Console.ReadLine();
+
+            while (input != "Last note")
+            {
+                Match match = Regex.Match(input, pattern);
+
+                if (match.Success)
+                {
+                    int geohashcodeLength = int.Parse(match.Groups["length"].Value);
+                    string geohashcode = match.Groups["code"].Value;
+                    int codeLength = geohashcode.Length;
+
+                    if (geohashcodeLength == codeLength)
+                    {
+                        string name = match.Groups["peakName"].Value;
+
+                        StringBuilder decryptedGeohashcode = new StringBuilder();
+
+                        for (int i = 0; i < name.Length; i++)
+                        {
+                            if (char.IsLetterOrDigit(name[i]))
+                            {
+                                decryptedGeohashcode.Append(name[i]);
+                            }
+                        }
+
+                        Console.WriteLine($"Coordinates found! {decryptedGeohashcode} -> {geohashcode}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nothing found!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Nothing found!");
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ 04. Final Exam - 14 April 2019 Group I/02. On the Way to Annapurna/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._On_the_Way_to_Annapurna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> stores = new Dictionary<string, List<string>>();
+
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                string[] tokens = input.Split("->");
+                string command = tokens[0];
+
+                if (command == "Add")
+                {
+                    string store = tokens[1];
+                    string[] items = tokens[2].Split(new char[] {','});
+
+                    if (!stores.ContainsKey(store))
+                    {
+                        stores.Add(store, new List<string>());
+                    }
+
+                    for (int i = 0; i < items.Length; i++)
+                    {
+                        stores[store].Add(items[i]);
+                    }
+                }
+                else if (command == "Remove")
+                {
+                    string store = tokens[1];
+                    if (stores.ContainsKey(store))
+                    {
+                        stores.Remove(store);
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine("Stores list:");
+            foreach (var store in stores
+                .OrderByDescending(x=>x.Value.Count)
+                .ThenByDescending(x=>x.Key))
+            {
+                Console.WriteLine(store.Key);
+                foreach (var item in store.Value)
+                {
+                    Console.WriteLine($"<<{item}>>");
+                }
+            }
+        }
+    }
+}
