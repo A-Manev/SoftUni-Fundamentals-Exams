3128f56c7d67cc714c2c4ac184292a6507d04f0c
A-Manev <52919486+A-Manev@users.noreply.github.com>
2019-11-25 14:05:57 +0200

    Final Exam - 06 April 2019
    (Demo) Technology Fundamentals Final Exam - 06 April 2019

---
+++ 03. Final Exam - 06 April 2019/01. Dictionary/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> definitions = new Dictionary<string, List<string>>();
+
+            List<string> allWords = new List<string>();
+
+            string[] input = Console.ReadLine().Split(" | ");
+
+            string[] someWords = Console.ReadLine().Split(" | ");
+
+            string endOrList = Console.ReadLine();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string[] pairsOfWordsAndDescriptions = input[i].Split(": ");
+
+                string word = pairsOfWordsAndDescriptions[0];
+                string definition = pairsOfWordsAndDescriptions[1];
+
+                if (!definitions.ContainsKey(word))
+                {
+                    allWords.Add(word);
+                    definitions.Add(word, new List<string>());
+                }
+
+                definitions[word].Add(definition);
+            }
+
+            for (int i = 0; i < someWords.Length; i++)
+            {
+                if (definitions.ContainsKey(someWords[i]))
+                {
+                    Console.WriteLine($"{someWords[i]}");
+
+                    foreach (var definition in definitions[someWords[i]].OrderByDescending(x=>x.Length))
+                    {
+                        Console.WriteLine($" -{definition}");
+                    }
+                }
+            }
+
+            if (endOrList == "List")
+            {
+                allWords.Sort();
+                Console.WriteLine(string.Join(" ", allWords));
+            }
+        }
+    }
+}
---
+++ 03. Final Exam - 06 April 2019/02. Deciphering/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _02._Deciphering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string encryptedString = Console.ReadLine();
+
+            string[] lettersOrSubstrings = Console.ReadLine().Split();
+
+            string pattern = @"^[d-z\{\}\|#]+$";
+
+            Match match = Regex.Match(encryptedString, pattern);
+
+            if (match.Success)
+            {
+                string oldLetters = lettersOrSubstrings[0];
+                string newLetters = lettersOrSubstrings[1];
+
+                StringBuilder decodedString = new StringBuilder();
+
+                for (int i = 0; i < encryptedString.Length; i++)
+                {
+                    decodedString.Append((char)(encryptedString[i] - 3));
+                }
+
+                decodedString.Replace(oldLetters, newLetters);
+
+                Console.WriteLine(decodedString.ToString());
+            }
+            else
+            {
+                Console.WriteLine("This is not the book you are looking for.");
+            }
+        }
+    }
+}
