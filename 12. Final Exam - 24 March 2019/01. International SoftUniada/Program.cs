a83c85df341f963ed2245a85204ba31f4aed44ff
Aleksandar Manev <52919486+A-Manev@users.noreply.github.com>
2019-12-03 18:49:15 +0200

    Final Exam - 24 March 2019
    Programming Fundamenta Additional Retake Exam - 24 March 2019

---
+++ 12. Final Exam - 24 March 2019/01. International SoftUniada/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _01.International_SoftUniada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> contestants = new Dictionary<string, List<string>>();
+
+            Dictionary<string, int> countries = new Dictionary<string, int>();
+
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                string[] tokens = input.Split(" -> ");
+
+                string countryName = tokens[0];
+                string contestantName = tokens[1];
+                int contestantPoints = int.Parse(tokens[2]);
+
+                if (!countries.ContainsKey(countryName))
+                {
+                    countries.Add(countryName, 0);
+                }
+
+                countries[countryName] += contestantPoints;
+
+                if (!contestants.ContainsKey(countryName))
+                {
+                    contestants.Add(countryName, new List<string>());
+                }
+
+                if (!contestants[countryName].Contains(contestantName))
+                {
+                    contestants[countryName].Add($"{contestantName} -> {contestantPoints}");
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var country in countries.OrderByDescending(x => x.Value))
+            {
+                Console.WriteLine($"{country.Key}: {country.Value}");
+
+                foreach (var contestant in contestants)
+                {
+                    if (country.Key == contestant.Key)
+                    {
+                        Console.WriteLine(contestant.Value);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 12. Final Exam - 24 March 2019/02. Emoji Sumator/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string patternTwo = @"(?<=[\s])\:(?<emoji>[a-z]{4,})\:(?=[\s,.!?])";
+
+            List<string> emojis = new List<string>();
+            List<string> emojisForPrint = new List<string>();
+
+            string input = Console.ReadLine();
+
+            List<int> emojiCode = Console.ReadLine().Split(":").Select(int.Parse).ToList();
+
+            MatchCollection matchedEmojis = Regex.Matches(input, patternTwo);
+
+            foreach (Match emoji in matchedEmojis)
+            {
+                string text = emoji.Groups["emoji"].Value;
+
+                if (IsAllCharEnglish(text))
+                {
+                    emojis.Add(emoji.Groups["emoji"].Value);
+
+                    emojisForPrint.Add($":{emoji.Groups["emoji"].Value}:");
+                }
+            }
+
+            int emojiPower = SummingTheASCIIValuesFromEmojiLetters(emojis);
+
+            string emojiName = string.Empty;
+
+            for (int i = 0; i < emojiCode.Count; i++)
+            {
+                emojiName += ((char)emojiCode[i]).ToString();
+            }
+
+            if (emojisForPrint.Count == 0)
+            {
+                Console.WriteLine("Total Emoji Power: 0");
+                return;
+            }
+
+            if (emojis.Contains(emojiName))
+            {
+                emojiPower *= 2;
+
+                Console.WriteLine($"Emojis found: {string.Join(", ", emojisForPrint)}");
+                Console.WriteLine($"Total Emoji Power: {emojiPower}");
+            }
+            else
+            {
+                Console.WriteLine($"Emojis found: {string.Join(", ", emojisForPrint)}");
+                Console.WriteLine($"Total Emoji Power: {emojiPower}");
+            }
+
+        }
+
+        private static int SummingTheASCIIValuesFromEmojiLetters(List<string> emojis)
+        {
+            int totalSum = 0;
+
+            for (int i = 0; i < emojis.Count; i++)
+            {
+                string currentEmoji = emojis[i];
+
+                for (int j = 0; j < currentEmoji.Length; j++)
+                {
+                    totalSum += currentEmoji[j];
+                }
+            }
+
+            return totalSum;
+        }
+
+        public static bool IsAllCharEnglish(string Input)
+        {
+            foreach (var item in Input.ToCharArray())
+            {
+                if (!char.IsLower(item) && !char.IsUpper(item) && !char.IsDigit(item) && !char.IsWhiteSpace(item))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
